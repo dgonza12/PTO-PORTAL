eab67adb452547f2394a7008dac7a4d9f851cec8
Danny Gonzalez <danny@meruscase.com>
2020-01-15 13:51:54 -0800

    Add basic routing react stuff

--- src/pages/EditRequest.js
+++ src/pages/EditRequest.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import {
+  useParams
+} from "react-router-dom";
 
 export default class EditRequest extends Component {
+
     render() {
+    	const { id } = this.props.match.params;
+    	const params = new URLSearchParams(this.props.location.search);
+    	const foo = params.get('foo');
+
         return (
             <div>
-                <p>Welcome to Todos List Component!!</p>
+                <p>{id} {foo}</p>
             </div>
         )
     }
--- src/pages/RequestList.js
+++ src/pages/RequestList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class RequestList extends Component {
     render() {
         return (
-            <div>
-                <p>Welcome to Todos List Component!!</p>
-            </div>
+            <table className="table table-dark">
+            	<thead>
+            		<tr>
+            			<th>
+            			<Link to="/edit/0?foo=bar">ID</Link>
+            			</th>
+            			<th>STATUS</th>
+            			<th>WORKER</th>
+            			<th>DESCRIPTION</th>
+            			<th>START DATE</th>
+            			<th>END DATE</th>
+            		</tr>
+            	</thead>
+            </table>
         )
     }
 }
